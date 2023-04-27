3e02ec0289cd6395a83ef9507acc809676b5750c
Tycho Cinquini <tycho.cinquini@colorado.edu>
2023-04-27 10:51:07 -0600

    More debugging

--- src/mapOptmization.cpp
+++ src/mapOptmization.cpp
@@ -1788,6 +1788,7 @@ public:
 
                 // Publish CF message
                 std::cout << "Sending data to tracking algorithm..." << endl;
+                ros::Duration(0.5).sleep();
                 pubCF.publish(CF);
             }
         }
