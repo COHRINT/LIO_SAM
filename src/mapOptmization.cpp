40194363a5571dc66cf8e0ac4c274b4eb8f74e89
ofe.dagan <ofer.dagan@colorado.edu>
2024-05-24 11:14:12 -0600

    changet std_msg to int64

--- src/mapOptmization.cpp
+++ src/mapOptmization.cpp
@@ -6,7 +6,8 @@
 #include <Eigen/Dense>
 
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+// #include "std_msgs/String.h"
+#include "std_msgs/Int64.h"
 
 #include "utility.h"
 #include "lio_sam/cloud_info.h"
@@ -239,7 +240,7 @@ public:
         pubCF                 = nh.advertise<lio_sam::ChannelFilter>("SLAM_CF_chatter_"+ns, 1);
         subCFRequest          = nh.subscribe<lio_sam::SLAMRequest>("SLAM_chatter_"+ns, 1, &mapOptimization::SLAMRequestHandler, this, ros::TransportHints().tcpNoDelay());
 
-        subBoss               = nh.subscribe<std_msgs::String>("boss", 1, &mapOptimization::bossHandler, this, ros::TransportHints().tcpNoDelay());
+        subBoss               = nh.subscribe<std_msgs::Int64>("boss", 1, &mapOptimization::bossHandler, this, ros::TransportHints().tcpNoDelay());
 
         downSizeFilterCorner.setLeafSize(mappingCornerLeafSize, mappingCornerLeafSize, mappingCornerLeafSize);
         downSizeFilterSurf.setLeafSize(mappingSurfLeafSize, mappingSurfLeafSize, mappingSurfLeafSize);
@@ -298,7 +299,7 @@ public:
         key_idx = msgIn->timeSteps;
     }
 
-    void bossHandler(const std_msgs::String::ConstPtr& msgIn) {};
+    void bossHandler(const std_msgs::Int64::ConstPtr& msgIn) {};
 
     void laserCloudInfoHandler(const lio_sam::cloud_info::ConstPtr& msgIn)
     {
